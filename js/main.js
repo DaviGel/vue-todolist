76dc2e9271f1273df09741ee0c08b57bbf4d8ec9
DaviGel <geloso.davide@gmail.com>
2023-11-28 15:48:43 +0100

    feat: milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      todos: [
+        {
+          text: 'Fare una passeggiata al parco',
+          done: false,
+        },
+        {
+          text: 'Magiare un gelato',
+          done: true,
+        },
+        {
+          text: 'Fare sport',
+          done: true,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    checkStatus(check) {
+      if (check === true) return 'text-decoration-line-through';
+      else return 'text-decoration-none';
+    },
+  },
+}).mount('#app');
